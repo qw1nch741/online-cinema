3e31f7ce69f7fbb0dc9b4ddeaf992075891453ec
Kyryl <sautakiki.com@gmail.com>
2026-07-01 17:27:05 +0300

    feat(auth): configure security constants and update project dependencies

--- src/services/auth/security.py
+++ src/services/auth/security.py
@@ -1,5 +1,11 @@
 import bcrypt
+import jwt
+from datetime import datetime, timedelta, timezone
 
+# In production, these must come from your configuration settings or environment variables!
+SECRET_KEY = "UCEbmHgVH5DWTohtWXiR5E2vnUL6sWakHSjcxlVQyJh"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
 
 def hash_password(password: str) -> str:
     password_bytes = password.encode('utf-8')
@@ -12,4 +18,11 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     plain_bytes = plain_password.encode('utf-8')
     hashed_bytes = hashed_password.encode('utf-8')
     return bcrypt.checkpw(plain_bytes, hashed_bytes)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
